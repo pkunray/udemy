4c2ec62f3ba0548c9efee54f4e129c4685dc5b97
pkunray <pkunray@gmail.com>
2023-10-28 23:55:29 +0200

    feat: assignments for data types

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -5,3 +5,12 @@ let population = 1400;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+// undefined
+console.log(typeof language);
