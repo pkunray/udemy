bc7de8c47c444eef4060d07410d694d0c9080ccf
pkunray <pkunray@gmail.com>
2023-11-10 22:37:00 +0100

    feat: code challenge #1

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -35,3 +35,44 @@ let populationPotugal = 11000000;
 let description;
 description = "Portugal is in Europe, and its " + populationPotugal / 1000000 + " million people speak portuguese";
 console.log(description);
+
+// Coding Challenge #1
+/* 
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+*/
+
+
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.95;
+
+/*
+let massMark = 95;
+let heightMark = 1.88;
+
+let massJohn = 85;
+let heightJohn = 1.76;
+*/
+
+let BMIMark = massMark / heightMark ** 2;
+let BMIJohn = massJohn / heightJohn ** 2;
+
+let markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
